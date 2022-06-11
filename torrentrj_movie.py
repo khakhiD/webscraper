7131ed96997aade68d4c13f8c52c499d861614b5
khakhiD <bfwanso@naver.com>
2022-06-12 02:16:14 +0900

    commit

--- torrentrj_movie.py
+++ torrentrj_movie.py
@@ -1,60 +1,62 @@
+from datetime import datetime
 from matplotlib import dates
 import requests
 from bs4 import BeautifulSoup as bs
 import pandas as pd
 
+TIME = datetime.now()
+URL = 'https://torrentrj39.com/v-1?page='
+PATH = 'webscraper/xlsx/'
+FNAME = 'TorrentRJ_movie_' + TIME.strftime('%y-%m-%d_%H-%M') + '.xlsx'
 titles = []
 size = []
 date = []
 links = []
 download = []
 
-for page_num in range(10):
-    url = f'https://torrentrj39.com/v-1?page={page_num+1}'
-    page = requests.get(url)
-    soup = bs(page.text, 'html.parser')
-    elements = soup.select('div.topic-item')
+def save_xsxl():
+    df = pd.DataFrame()
+    df['제목'] = titles
+    df['용량'] = size
+    df['날짜'] = date
+    df['링크'] = links
+    df['다운'] = download
     
-    for i, e in enumerate(elements, 1):
-        t = e.select_one('div.ti2 > a.tit').attrs['title']
-        s = e.select_one('div.ti3').text
-        d = e.select_one('div.ti4').text
-        l = 'https://torrentrj39.com'+e.select_one('a.tit').attrs['href']
+    df.to_excel(PATH + FNAME, sheet_name = 'new_movie', encoding = 'utf-8', index = False)
 
-        titles.append(t)
-        size.append(s)
-        date.append(d)
-        links.append(l)
+def array_append(a,b,c,d):
+    titles.append(a)
+    size.append(b)
+    date.append(c)
+    links.append(d)
+    dl_soup = bs(requests.get(d).text,'html.parser')
+    if (dl_soup.select_one('a[class="btn main_bg btn-sm"]')) is None:
+        e = 'none'
+        download.append('-')
+        return
+    else:
+        e = 'https://torrentrj39.com' + dl_soup.select_one('a[class="btn main_bg btn-sm"]').attrs['href']
+        download.append(e)
+
+    print(str(a)+'\n'+str(b)+'\n'+str(c)+'\n'+str(d)+'\n'+str(e)+'\n')
+
+def page_scraping(num):
+    for page_num in range(num):
+        url = URL + str(page_num+1)
+        req = requests.get(url)
+        soup = bs(req.text, 'html.parser')
+        elements = soup.select('div.topic-item')
         
-        l_soup = bs(requests.get(l).text,'html.parser')
-        
-        if (l_soup.select_one('a[class="btn main_bg btn-sm"]')) is None:
-            print("***************해당 게시글은 다운로드가 없음**************\n")
-            download.append('-')
-            continue
-        else:
-            dl = 'https://torrentrj39.com'+l_soup.select_one('a[class="btn main_bg btn-sm"]').attrs['href']
-            download.append(dl)
-            
-        print("\n페이지:"+str(page_num+1)+"번호:"+str(i))
-        print(str(t)+" ")
-        print(str(s)+" ")
-        print(str(d)+" ")
-        print(str(l)+" ")
-        print(str(dl)+" ")
+        for i, e in enumerate(elements, 1):
+            t = e.select_one('div.ti2 > a.tit').attrs['title']
+            s = e.select_one('div.ti3').text
+            d = e.select_one('div.ti4').text
+            l = 'https://torrentrj39.com'+e.select_one('a.tit').attrs['href']
+            print("페이지: "+ str(page_num+1)+"\t번호: "+str(i))
+            array_append(t,s,d,l)
+    save_xsxl()
+
+if __name__ == "__main__":
+    page_scraping(5)
 
 
-df = pd.DataFrame()
-df['제목'] = titles
-df['용량'] = size
-df['날짜'] = date
-df['링크'] = links
-df['다운'] = download
-
-df.to_excel('xlsx/torrentrj_movie.xlsx',
-            sheet_name='Sheet1',
-            encoding='utf-8',
-            na_rep = '',
-            inf_rep = '',
-            index = False
-)
